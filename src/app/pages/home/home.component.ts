72bd694aac16397a41634ea3a68580278679b7ac
Perillos <valero.ce@gmail.com>
2022-09-16 11:26:29 +0200

    table view

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -11,12 +11,20 @@ import { ApiService } from './../../service/api.service';
 })
 export class HomeComponent implements OnInit {
   public TareasList : TareasInterface[] = []
+  public TypeList : TareasInterface[] = []
+  public StateList : TareasInterface[] = []
 
   constructor(private apiService: ApiService) { }
 
   ngOnInit(): void {
      this.apiService.getTareas().subscribe((data: any) =>{
       this.TareasList = data.data;
+    })
+     this.apiService.getTipos().subscribe((data: any) =>{
+      this.TypeList = data.data;
+    })
+    this.apiService.getEstados().subscribe((data: any) =>{
+      this.StateList = data.data;
     })
   }
   
