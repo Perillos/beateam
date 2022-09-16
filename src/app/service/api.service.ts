9776b7ce94757bb961abfc3160b3b4df18537c9f
Perillos <valero.ce@gmail.com>
2022-09-16 09:52:32 +0200

    FEAT: request to the api

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -1,22 +1,56 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment'
+// use jsSHA to hash
+import  jsSHA  from  "jssha"
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  public BaseURL: string = 'https://www.azurglobal.es/apiPracticas/'
+  public BaseURL: string = environment.baseURL
+  public auth: string = environment.PARTEPRIVADA
 
   constructor(private httpClient: HttpClient) { }
 
+  // get hash of the day
+  public getAuth() {
+    const timeDate = Date.now();
+    const today = new Date(timeDate);
+    const day =  today.getDate()
+    const dayNum = day.toString().length==1 ? 0 + day.toString() :  day.toString()
+    const month =  today.getMonth() + 1
+    const monthNum = month.toString().length==1 ? 0 + month.toString() :  month.toString()
+    const year =  today.getFullYear()
+    const ymd = `${year}${monthNum}${dayNum}`
+    const keyP = environment.PARTEPRIVADA + ymd
+    const shaObj = new jsSHA("SHA-384", "TEXT", { encoding: "UTF8" });
+    shaObj.update(keyP);
+    const hash = shaObj.getHash("HEX");
+    return hash
+  }
 
 
   public getTareas() {
     return this.httpClient.get(this.BaseURL, {
-      headers: {'X-Auth':'cef900641a4c78146d16ad134040de77ff29b0fbc9844d07596b246b3a8bb5c16058cca19c52218dc29e08bfdfaca885','funcion':'getTareas'}
+      headers: {
+        'X-Auth': this.getAuth(),
+        'funcion':'getTareas'}
     })
   }
-  // public getTareas() {
-  //   return this.httpClient.get(this.BaseURL)
-  // }
+  public getTipos() {
+    return this.httpClient.get(this.BaseURL, {
+      headers: {
+        'X-Auth': this.getAuth(),
+        'funcion':'getTipos'}
+    })
+  }
+  public getEstados() {
+    return this.httpClient.get(this.BaseURL, {
+      headers: {
+        'X-Auth': this.getAuth(),
+        'funcion':'getEstados'}
+    })
+  }
 }
